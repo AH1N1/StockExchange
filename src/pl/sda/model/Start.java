9891d7808786ad019b06c97c564c57db668f001d
Daniel.B <daniel.bogucki2@gmail.com>
2017-06-23 20:01:37 +0200

    Initializing StockExchange and filling List<Company> Companies from file.txt

--- src/pl/sda/model/Start.java
+++ src/pl/sda/model/Start.java
@@ -1,18 +1,9 @@
 package pl.sda.model;
 
-import java.io.IOException;
-
 public class Start {
 
 	public static void main(String[] args) {
 		StockExchange Exchange = new StockExchange();
-		
-		Company Apple = new Company("Apple", "https://www.quandl.com/api/v3/datasets/WIKI/AAPL.csv");
-		try {
-			Apple.downloadCSV();
-		} catch (IOException e) {
-			System.out.println(e.getMessage());
-		}
 	}
 
 }
--- src/pl/sda/model/StockExchange.java
+++ src/pl/sda/model/StockExchange.java
@@ -1,11 +1,41 @@
 package pl.sda.model;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.omg.CORBA.PUBLIC_MEMBER;
+
 public class StockExchange {
 	private List<Company> Companies = new ArrayList<>();
-	
-	public StockExchange(){
+
+	public StockExchange() {
+
+		try {
+			// Creating new file
+			BufferedReader br = new BufferedReader(new FileReader("file.txt"));
+
+			String name = br.readLine();
+			String downloadURL = br.readLine();
+			
+			int i = 0;
+			
+			while (!(name == null)){
+				
+				// Adding company and download .csv
+				Companies.add(new Company(name, downloadURL));
+				Companies.get(i).downloadCSV();;
+				i++;
+				
+				name = br.readLine();
+				downloadURL = br.readLine();
+			}
+
+			br.close();
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 }

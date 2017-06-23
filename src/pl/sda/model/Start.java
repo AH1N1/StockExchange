a7d2e6739eb9a8bbbe01040f661fd46d96636d33
Daniel.B <daniel.bogucki2@gmail.com>
2017-06-23 20:14:19 +0200

    Minor change in main

--- src/pl/sda/model/Start.java
+++ src/pl/sda/model/Start.java
@@ -3,7 +3,7 @@ package pl.sda.model;
 public class Start {
 
 	public static void main(String[] args) {
-		StockExchange Exchange = new StockExchange();
+		StockExchange stockExchange = new StockExchange();
 	}
 
 }
